560fee7a9e1168e92a954fd53d8c87eb0373cf8a
Daniel Uzochukwu Udo <danieluzochukwu0000@gmail.com>
2024-01-26 01:09:35 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from sys import argv
+from pytube import YouTube
+
+import os.path
+
+#check if any path was given in an argument
+if len(argv) > 1:
+    path = argv[1]
+#if non was given add a folder path
+else:
+    path = "/Users/ADMIN/Documents"
+
+#Add the YouTube video link
+yt = YouTube('https://youtu.be/vEQ8CXFWLZU')
+
+title = yt.title
+
+views = yt.views
+
+print("Title : " ,title , "Views : " ,views)
+
+#Get the video resoluction
+yr = yt.streams.get_highest_resolution()
+
+#Call the download API
+yr.download(path)
+
